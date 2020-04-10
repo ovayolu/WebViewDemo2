e5b7054c3a5c157adc4cdc92f568e6000fbe740d
Birkan Ovayolu <ovayolu@gmail.com>
2020-04-10 18:12:38 +0100

    Initial Commit

---
+++ WebViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewDemo
+//
+//  Created by Birkan Ovayolu on 10/04/2020.
+//  Copyright © 2020 Aris Education Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
