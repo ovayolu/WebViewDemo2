3bbe97aca60e72c10ea3ca60b248d870a7559fd3
Birkan Ovayolu <ovayolu@gmail.com>
2020-04-10 19:08:44 +0100

    First Test

--- WebViewDemo/ViewController.swift
+++ WebViewDemo/ViewController.swift
@@ -6,13 +6,37 @@
 //  Copyright © 2020 Aris Education Technologies. All rights reserved.
 //
 
-import UIKit
 
-class ViewController: UIViewController {
+import UIKit
+import WebKit
+
+
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 1
+        let url = URL(string: "http://denizyildiziyayinlari.com/app/home.html")!
+        webView.load(URLRequest(url: url))
+          
+        // 2
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    title = webView.title
     }
 
 
